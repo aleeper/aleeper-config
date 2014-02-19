d60c02387e158c165cd350beec88fc310c58ccf1
Adam Leeper <aleeper@hidof.com>
2014-02-19 10:07:58 -0800

    create script for setting up the machine

---
+++ setup_machine.sh
@@ -0,0 +1,25 @@
+echo -e "\e[1;32mInstalling packages...\e[0m"
+
+sudo apt-get update
+
+sudo apt-get install \
+audacity    \
+ccache      \
+colorgcc    \
+git         \
+meshlab     \
+vim         \
+
+echo -e "\e[1;32mCopying configuration files...\e[0m"
+for f in config/*; do
+  dest="../.`basename $f`";
+  echo "Copying $f to $dest"
+  cp $f $dest;
+done
+
+echo -e "\e[1;32mCloning repositories...\e[0m"
+if [ -d ./when-changed ]; then
+  echo "when-changed is already checked out, skipping"
+else
+  git clone https://github.com/aleeper/when-changed.git
+fi
