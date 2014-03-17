40d89b470faf0da307d36a5a9dec8de35acee84e
Adam Leeper <aleeper@hidof.com>
2014-03-16 19:59:29 -0700

    add repository config; add google translate

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,5 +1,20 @@
 echo -e "\e[1;32mUpdating packages...\e[0m"
-sudo add-apt-repository ppa:git-core/ppa
+
+PPA_INSTALLED=`list-repositories | grep -o "ppa:\S*$"`
+
+PPA_LIST=`cat packages/apt-ppa`
+
+for ppa in $PPA_LIST; do
+  ppa=`echo $ppa | grep -o "[^']*"`
+  if [[ $PPA_INSTALLED != *$ppa* ]]; then
+    echo "Adding PPA: $ppa"
+    sudo apt-add-repository $ppa
+  else
+    echo "$ppa is already in the apt sources list."
+  fi
+done
+
+
 sudo apt-get update
 
 echo -e "\e[1;32mRunning a dist-upgrade...\e[0m"
@@ -24,8 +39,20 @@ for f in config/*; do
 done
 
 echo -e "\e[1;32mCloning repositories...\e[0m"
+
 if [ -d ./when-changed ]; then
   echo "when-changed is already checked out, skipping"
 else
   git clone https://github.com/aleeper/when-changed.git
 fi
+
+if [ -d ./google-translate-cli-master ]; then
+  echo "google-translate-cli already exists, skipping"
+else
+  wget https://github.com/soimort/google-translate-cli/archive/master.tar.gz
+  tar -xvf master.tar.gz
+  cd google-translate-cli-master/
+  sudo make install
+  cd ..
+fi
+
