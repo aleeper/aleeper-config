d5571149729d404c3cce69161d7dafad765197e4
Adam Leeper <adamleeper@gmail.com>
2020-07-14 13:26:26 -0700

    fix colors

--- bash-settings.sh
+++ bash-settings.sh
@@ -3,7 +3,14 @@ echo "Loading BASH settings!"
 alias reconfig='. ~/.bashrc'
 alias editconfig='vim ~/.bashrc'
 
+#export MYPS='$(echo -n "${PWD/#$HOME/~}" | awk -F "/" '"'"'{if (length($0) > 14) { if (NF>4) print $1 "/" $2 "/.../" $(NF-1) "/" $NF; else if (NF>3) print $1 "/" $2 "/.../" $NF; else print $1 "/.../" $NF; } else print $0;}'"'"')'
 #PS1='$(eval "echo ${MYPS}")$ '
 #PROMPT_DIRTRIM=2
 #PS1="\u@\h:\w\n$"
-#PS1="\u\$ "
+# Note: \w is full path, \W is just current dir.
+
+GREEN="\[\033[01;32m\]"
+WHITE="\[\033[01;37m\]"
+YELLOW="\[\033[01;33m\]"
+
+PS1="$GREEN\u$WHITE:$YELLOW\W$WHITE\n\$ "
--- shortcuts.sh
+++ shortcuts.sh
@@ -52,7 +52,6 @@ export PATH=$config_dir/bin:$PATH
 ###############################################################################
 # Modify command prompt formatting
 ###############################################################################
-#export MYPS='$(echo -n "${PWD/#$HOME/~}" | awk -F "/" '"'"'{if (length($0) > 14) { if (NF>4) print $1 "/" $2 "/.../" $(NF-1) "/" $NF; else if (NF>3) print $1 "/" $2 "/.../" $NF; else print $1 "/.../" $NF; } else print $0;}'"'"')'
 
 ### Don't remember if I need this.
 #ssh-agent sh -c 'ssh-add < /dev/null && bash'
